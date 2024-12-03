dad9833819ea9cc5df9f5e4714c14381e0813347
Arthur <arthur@cybrco.ai>
2024-12-03 15:35:20 -0500

    added navigation control

--- src/components/map/MapInstance.tsx
+++ src/components/map/MapInstance.tsx
@@ -2,7 +2,7 @@
 
 import type { FC } from "react"
 import { useEffect } from "react"
-import Map, {FullscreenControl } from "react-map-gl"
+import Map, { FullscreenControl, NavigationControl } from "react-map-gl"
 import "mapbox-gl/dist/mapbox-gl.css"
 import { getPokemonCenterLocations } from "@/lib/map/locations"
 import type { PokemonCenterLocation } from "@/lib/types/locations"
@@ -43,6 +43,7 @@ export const MapInstance: FC = () => {
       <PokemonCenterLocationPins />
       <LocationPinPopUps />
       <FullscreenControl position="bottom-right" />
+      <NavigationControl position="bottom-right" />
     </Map>
   )
 }
