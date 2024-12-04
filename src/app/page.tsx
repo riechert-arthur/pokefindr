f227376ad86f35a52516a5407f9a563369dc02a8
Arthur <arthur@cybrco.ai>
2024-12-03 21:02:14 -0500

    fix: displays metadata tags now
    Moves declaration of meta tags from the jsx description to an object
    stored as a JSON file.
    
    Closes #8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,13 @@
 import type { FC } from "react"
 import { MapInstance } from "@/components/map/MapInstance"
 import { TopNavBar } from "@/components/TopNavBar"
-import Head from "next/head"
+import homePageMetadata  from "./metadata.json"
+
+export const metadata = homePageMetadata
 
 const HomePage: FC = () => {
   return (
     <>
-      <Head>
-        <title>
-          Find all Pokemon Vending Machine Locations - Free Map | PokeFindr
-        </title>
-        <meta
-          name="description"
-          content="See a map with all the Pokemon Vending Machine Locations. Easily send the directions to Google Maps."
-          key="desc"
-        />
-        <meta
-          name="keywords"
-          content="Pokemon vending machines, Pokemon map, vending machine locations, Pokemon merchandise map, PokeFindr"
-        />
-        <meta
-          name="author"
-          content="PokeFindr Developer"
-        />
-        <meta property="og:title" content="Find all Pokemon Vending Machine Locations | PokeFindr" />
-        <meta
-          property="og:description"
-          content="Explore a comprehensive map of Pokemon Vending Machine Locations. Easily find directions to your nearest vending machine!"
-        />
-        <meta
-          name="robots"
-          content="index, follow"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta name="google-adsense-account" content="ca-pub-5624613492216779" />
-      </Head>
       <TopNavBar />
       <MapInstance />
     </>
