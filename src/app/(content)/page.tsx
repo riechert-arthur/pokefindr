942d7b92c9b0df5ee0fc3d6cae30101e332e89c5
arthurriechert <ariechert3@gatech.edu>
2024-12-16 21:09:24 -0500

    fix: adds display name

--- src/app/(content)/page.tsx
+++ src/app/(content)/page.tsx
@@ -3,17 +3,26 @@ import Link from "next/link"
 import homePageMetadata from "./metadata.json"
 import dynamic from "next/dynamic"
 import { LoadSpinner } from "@/components/LoadSpinner"
+import type { FAQSectionProps } from "./FAQSection"
 
 export const metadata = homePageMetadata
 
-const FAQSection = dynamic(async () => {
+type LazyFAQSectionProps = Omit<FAQSectionProps, 'faqs'>;
+
+const FAQSection = dynamic<LazyFAQSectionProps>(async () => {
   const [FAQSectionModule, faqs] = await Promise.all([
     import('./FAQSection'),
     import('./faqs.json'),
   ])
 
   const FAQSectionComponent = FAQSectionModule.default;
-  return (props: any) => <FAQSectionComponent {...props} faqs={faqs.default} />;
+
+  const LazyFAQSection: React.FC<LazyFAQSectionProps> = (props) => {
+    return <FAQSectionComponent {...props} faqs={faqs.default} />
+  }
+
+  LazyFAQSection.displayName = "LazyFAQSection";
+  return LazyFAQSection
 }, {
   loading: () => <LoadSpinner text="Loading FAQs..." />,
 })
