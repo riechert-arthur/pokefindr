a8652f7d8a3e1d260ecb4d11d3d4bd00d9b1a3a9
arthurriechert <ariechert3@gatech.edu>
2024-12-18 00:20:24 -0500

    fix: loads FAQ from client component

--- src/app/(content)/page.tsx
+++ src/app/(content)/page.tsx
@@ -1,36 +1,10 @@
 import type { FC } from "react"
 import Link from "next/link"
 import homePageMetadata from "./metadata.json"
-import dynamic from "next/dynamic"
-import { LoadSpinner } from "@/components/LoadSpinner"
-import type { FAQSectionProps } from "./FAQSection"
 import Head from "next/head"
+import { FAQLoader } from "./FAQLoader"
 
 export const metadata = homePageMetadata
-
-type LazyFAQSectionProps = Omit<FAQSectionProps, "faqs">
-
-const FAQSection = dynamic<LazyFAQSectionProps>(
-  async () => {
-    const [FAQSectionModule, faqs] = await Promise.all([
-      import("./FAQSection"),
-      import("./faqs.json"),
-    ])
-
-    const FAQSectionComponent = FAQSectionModule.default
-
-    const LazyFAQSection: React.FC<LazyFAQSectionProps> = (props) => {
-      return <FAQSectionComponent {...props} faqs={faqs.default} />
-    }
-
-    LazyFAQSection.displayName = "LazyFAQSection"
-    return LazyFAQSection
-  },
-  {
-    loading: () => <LoadSpinner text="Loading FAQs..." />,
-    ssr: false,
-  }
-)
 
 const HeroSection: FC = () => {
   return (
@@ -160,7 +134,7 @@ const LandingPage: FC = () => {
       </Head>
       <div className="isolate">
         <HeroSection />
-        <FAQSection />
+        <FAQLoader />
       </div>
     </>
   )
